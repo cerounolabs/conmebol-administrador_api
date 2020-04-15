7c146955fe2bc689fdf27ffef5c111cde523259e
Christian Zelaya - PC-01 <christian@cerouno.com.py>
2020-04-15 18:10:56 -0400

    Fix Marcacion

--- appv1_sap/get.php
+++ appv1_sap/get.php
@@ -2409,10 +2409,10 @@
                 $stmtMSSQL->execute([$val01]);
                 
                 while ($rowMSSQL = $stmtMSSQL->fetch()) {
-                    $marcacion_entrada_oficina_hora = (strlen(trim($rowMSSQL['marcacion_entrada_oficina_hora'])) > 999 ? date("H:i") : date("H:i", strtotime('0'.trim($rowMSSQL['marcacion_entrada_oficina_hora']))));
-                    $marcacion_salida_almuerzo_hora = (strlen(trim($rowMSSQL['marcacion_salida_almuerzo_hora'])) > 999 ? date("H:i") : date("H:i", strtotime('0'.trim($rowMSSQL['marcacion_salida_almuerzo_hora']))));
-                    $marcacion_entrada_almuerzo_hora= date('H:i');
-                    $marcacion_salida_oficina_hora  = date('H:i');
+                    $marcacion_entrada_oficina_hora = (strlen(trim($rowMSSQL['marcacion_entrada_oficina_hora'])) > 3 ? date('H:i') : date("H:i", strtotime('0'.trim($rowMSSQL['marcacion_entrada_oficina_hora']))));
+                    $marcacion_salida_almuerzo_hora = (strlen(trim($rowMSSQL['marcacion_salida_almuerzo_hora'])) > 3 ? date('H:i') : date("H:i", strtotime('0'.trim($rowMSSQL['marcacion_salida_almuerzo_hora']))));
+                    $marcacion_entrada_almuerzo_hora= (strlen(trim($rowMSSQL['marcacion_entrada_almuerzo_hora'])) > 3 ? date('H:i') : date("H:i", strtotime('0'.trim($rowMSSQL['marcacion_entrada_almuerzo_hora']))));
+                    $marcacion_salida_oficina_hora  = (strlen(trim($rowMSSQL['marcacion_salida_oficina_hora'])) > 3 ? date('H:i') : date("H:i", strtotime('0'.trim($rowMSSQL['marcacion_salida_oficina_hora']))));
 
                     $detalle    = array(
                         'marcacion_codigo'                          => $rowMSSQL['marcacion_codigo'],
