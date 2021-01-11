a4b2681b3e0216eb9ce136a074f8343a54c41231
CEROUNO_PC01 <christian@cerouno.com.py>
2021-01-11 17:48:25 -0300

    Feature: search colaborador SAP

--- appv1_sap/get.php
+++ appv1_sap/get.php
@@ -1841,8 +1841,11 @@
             LEFT OUTER JOIN [CSF].[dbo].[empleados_AxisONE] b ON a.CodCargoSuperior = b.CodigoCargo
             
             WHERE
-            (a.Estado = 'V' OR (a.Estado = 'N' AND (a.CedulaEmpleado = '798293' OR a.CedulaEmpleado = '1421530' OR a.CedulaEmpleado = '7951461' OR a.CedulaEmpleado = '574039' OR a.CedulaEmpleado = '17388982-7' OR a.CedulaEmpleado = '426942' OR a.CedulaEmpleado = '530962500'))) AND
-            (b.Estado = 'V' OR (b.Estado = 'N' AND (b.CedulaEmpleado = '798293' OR b.CedulaEmpleado = '1421530' OR b.CedulaEmpleado = '7951461' OR b.CedulaEmpleado = '574039' OR b.CedulaEmpleado = '17388982-7' OR b.CedulaEmpleado = '426942' OR b.CedulaEmpleado = '530962500')))";
+            (
+                (a.Estado = 'V' OR (a.Estado = 'N' AND (a.CedulaEmpleado = '798293' OR a.CedulaEmpleado = '1421530' OR a.CedulaEmpleado = '7951461' OR a.CedulaEmpleado = '574039' OR a.CedulaEmpleado = '17388982-7' OR a.CedulaEmpleado = '426942' OR a.CedulaEmpleado = '530962500'))) 
+                OR
+                (b.Estado = 'V' OR (b.Estado = 'N' AND (b.CedulaEmpleado = '798293' OR b.CedulaEmpleado = '1421530' OR b.CedulaEmpleado = '7951461' OR b.CedulaEmpleado = '574039' OR b.CedulaEmpleado = '17388982-7' OR b.CedulaEmpleado = '426942' OR b.CedulaEmpleado = '530962500')))
+            )";
 
         try {
             $connMSSQL  = getConnectionMSSQLv1();
